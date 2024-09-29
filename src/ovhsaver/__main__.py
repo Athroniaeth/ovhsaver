77a9151481c05eb947ae5c1c6a8fd3a375a2b557
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-09-30 00:37:11 +0200

    Refactor, render more modular for future testing

--- src/ovhsaver/__main__.py
+++ src/ovhsaver/__main__.py
@@ -1,8 +1,13 @@
-from datetime import datetime, tzinfo
+from datetime import datetime
+from os import PathLike
+from pathlib import Path
+from typing import Union
 
 import pytz
 from openstack import connection
+from openstack.compute.v2.server import Server
 from openstack.config import loader
+from openstack.connection import Connection
 
 from src.ovhsaver import CONFIG_PATH
 
@@ -28,40 +33,76 @@ def time_to_open(date: datetime) -> bool:
     return result
 
 
+def get_conn_openstack(
+        cloud_name: str = 'ovhcloud',
+        config_path: Union[str, PathLike] = None
+) -> Connection:
+    """
+    Get connection to OpenStack cloud
+
+    Args:
+        cloud_name (str): Name of the cloud to connect to
+        config_path (Union[str, PathLike]): Path to the clouds.yaml file
+
+    Returns:
+        Connection: Connection to OpenStack cloud
+    """
+    # Parse config path if given
+    if config_path is not None:
+        config_path = Path(config_path)
+
+        if not config_path.exists():
+            raise FileNotFoundError(f"File '{config_path}' not found")
+
+        config_path = f"{config_path}"
+
+    # Load OpenStack configuration from the specified clouds.yaml file
+    config = loader.OpenStackConfig(config_files=[config_path])
+
+    # Retrieve the cloud-specific configuration for 'ovhcloud'
+    cloud_config = config.get_one_cloud(cloud=cloud_name)
+
+    # Initialize the OpenStack connection using the specified cloud configuration
+    conn = connection.Connection(config=cloud_config)
+
+    return conn
+
+
+def handle_server(server: Server, conn: Connection):
+    """
+    Handle the server to start or stop it
+
+    Args:
+        server (Server): Server to handle
+        conn (Connection): Connection to OpenStack cloud
+    """
+    server = conn.compute.get_server(server.id)
+    server_is_online = (server.status == 'ACTIVE')
+    print(f"Server '{server.name}' is {"'online'" if server_is_online else "'offline'"}")
+
+    if time_is_online and not server_is_online:
+        print(f'\tStarting server {server.name}...\n')
+        conn.compute.start_server(server.id)
+        conn.compute.wait_for_server(server, status='ACTIVE', failures=['ERROR'], interval=60, wait=360)
+
+    elif not time_is_online and server_is_online:
+        print(f'\tStopping server {server.name}...\n')
+        conn.compute.stop_server(server)
+        conn.compute.wait_for_server(server, status='SHUTOFF', failures=['ERROR'], interval=60, wait=360)
+
+    else:
+        print("Nothing to do\n")
+
+
 if __name__ == "__main__":
     TIME_ZONE = pytz.timezone("Europe/Paris")
     TODAY = datetime.now(tz=TIME_ZONE)
 
-    # Load OpenStack configuration from the specified clouds.yaml file
-    config = loader.OpenStackConfig(config_files=[f'{CONFIG_PATH}'])
-
-    # Retrieve the cloud-specific configuration for 'ovhcloud'
-    cloud_config = config.get_one_cloud(cloud='ovhcloud')
-
-    # Initialize the OpenStack connection using the specified cloud configuration
-    conn = connection.Connection(config=cloud_config)
+    conn = get_conn_openstack(config_path=CONFIG_PATH)
 
     time_is_online = time_to_open(date=TODAY)
     print(f"{time_is_online=}\n")
 
     # List the available compute instances
-    for _server in conn.compute.servers():
-        server = conn.compute.get_server(_server.id)
-        server_is_online = (server.status == 'ACTIVE')
-
-        print(f"Server '{server.name}' is {"'online'" if server_is_online else "'offline'"}")
-
-        if time_is_online and not server_is_online:
-            print(f'\tStarting server {server.name}...')
-            # conn.compute.start_server(server.id)
-            # conn.compute.wait_for_server(server, status='ACTIVE', failures=['ERROR'], interval=60, wait=360)
-
-        elif not time_is_online and server_is_online:
-            print(f'\tStopping server {server.name}...')
-            # conn.compute.stop_server(server)
-            # conn.compute.wait_for_server(server, status='SHUTOFF', failures=['ERROR'], interval=60, wait=360)
-
-        else:
-            print("Nothing to do")
-
-        print()
+    for server in conn.compute.servers():
+        handle_server(server, conn)
