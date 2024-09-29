3b54a37a03838861209187fd40738c432dae86a5
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-09-29 21:28:52 +0200

    Add main code, start or stop server according to hour and day

--- src/ovhsaver/__main__.py
+++ src/ovhsaver/__main__.py
@@ -1,19 +1,65 @@
-import os
+from datetime import datetime
 
+import pytz
 from openstack import connection
 from openstack.config import loader
 
 from src.ovhsaver import CONFIG_PATH
 
-# Load OpenStack configuration from the specified clouds.yaml file
-config = loader.OpenStackConfig(config_files=[f'{CONFIG_PATH}'])
 
-# Retrieve the cloud-specific configuration for 'ovhcloud'
-cloud_config = config.get_one_cloud(cloud='ovhcloud')
+def is_online_time() -> bool:
+    """
+    Return if is online time or not
 
-# Initialize the OpenStack connection using the specified cloud configuration
-conn = connection.Connection(config=cloud_config)
+    Notes:
+        it's 'online' time if it's 8h00 - 19h00 (Paris TZ)
+        it's offline time if it's 19h00 - 8h00 (Paris TZ)
+    """
+    tz = pytz.timezone("Europe/Paris")
+    hour = datetime.now(tz=tz).hour
+    is_weekend = datetime.now().weekday() in [5, 6]  # Saturday or Sunday
 
-# List the available compute instances
-for server in conn.compute.servers():
-    print(server)
+    conditions = (
+        8 <= hour < 19,
+        not is_weekend
+    )
+
+    result = all(conditions)
+    print(f"{hour=} ; {is_weekend=}")
+    return result
+
+
+if __name__ == "__main__":
+    # Load OpenStack configuration from the specified clouds.yaml file
+    config = loader.OpenStackConfig(config_files=[f'{CONFIG_PATH}'])
+
+    # Retrieve the cloud-specific configuration for 'ovhcloud'
+    cloud_config = config.get_one_cloud(cloud='ovhcloud')
+
+    # Initialize the OpenStack connection using the specified cloud configuration
+    conn = connection.Connection(config=cloud_config)
+
+    time_is_online = is_online_time()
+    print(f"{time_is_online=}\n")
+
+    # List the available compute instances
+    for _server in conn.compute.servers():
+        server = conn.compute.get_server(_server.id)
+        server_is_online = (server.status == 'ACTIVE')
+
+        print(f"Server '{server.name}' is {"'online'" if server_is_online else "'offline'"}")
+
+        if time_is_online and not server_is_online:
+            print(f'\tStarting server {server.name}...')
+            # conn.compute.start_server(server.id)
+            # conn.compute.wait_for_server(server, status='ACTIVE', failures=['ERROR'], interval=60, wait=360)
+
+        elif not time_is_online and server_is_online:
+            print(f'\tStopping server {server.name}...')
+            # conn.compute.stop_server(server)
+            # conn.compute.wait_for_server(server, status='SHUTOFF', failures=['ERROR'], interval=60, wait=360)
+
+        else:
+            print("Nothing to do")
+
+        print()
